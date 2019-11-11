80391c0e15fba6a5cec78252d90894ce82bad0e4
Ievgeniya Knyazeva <knyazevaievgeniya@gmail.com>
2019-11-11 20:46:27 +0200

    Added FractionNumber Class - lecture7

--- src/test/java/com/hillel/lecture_7/FractionNumber.java
+++ src/test/java/com/hillel/lecture_7/FractionNumber.java
@@ -4,6 +4,49 @@ package com.hillel.lecture_7;
  * Created by alpa on 11/8/19
  */
 public class FractionNumber {
+    private long numberOne;
+    private long numberTwo;
 
+    public FractionNumber(long numberOne, long numberTwo) {
+        this.numberOne = numberOne;
+        this.numberTwo = numberTwo;
+    }
 
+    public long getNumberOne() {
+        return numberTwo;
+    }
+
+    public long getNumberTwo() {
+        return numberTwo;
+    }
+
+    public long fractionNumberPlus() {
+        return this.numberOne+this.numberTwo;
+    }
+
+    public long fractionNumberMinus() {
+        return this.numberOne-this.numberTwo;
+    }
+
+    public long fractionNumberMultiply() {
+        return this.numberOne*this.numberTwo;
+    }
+
+    public long fractionNumberDivide() {
+        if (this.numberTwo==0) {
+            System.out.println("It's forbidden to Divide to Zero!!!");
+            return 0;
+        }
+        else {
+            return this.numberOne / this.numberTwo;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "FractionNumber{" +
+                "numberOne=" + numberOne +
+                ", numberTwo=" + numberTwo +
+                '}';
+    }
 }
--- src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
+++ src/test/java/com/hillel/lecture_7/FractionNumbersTestTask.java
@@ -1,5 +1,9 @@
 package com.hillel.lecture_7;
 
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 /**
  * Write class to represent FractionNumbers
  * This class should contain two fields.
@@ -10,9 +14,32 @@ package com.hillel.lecture_7;
  */
 public class FractionNumbersTestTask {
 
+    private FractionNumber fractionNumber = new FractionNumber(9, 3);
+
+    @Test
+    public void testPlus() {
+        assertEquals(fractionNumber.fractionNumberPlus(), 12);
+    }
 
 
+    @Test
+    public void testMinus() {
+        assertEquals(fractionNumber.fractionNumberMinus(), 6);
+    }
 
+    @Test
+    public void testMultiply() {
+        assertEquals(fractionNumber.fractionNumberMultiply(), 27);
+    }
 
+    @Test
+    public void testDivide() {
+        assertEquals(fractionNumber.fractionNumberDivide(), 3);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(fractionNumber.toString(),"FractionNumber{numberOne=9, numberTwo=3}");
+    }
 
 }
